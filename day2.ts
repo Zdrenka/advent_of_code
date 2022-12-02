39a8f3b4330f80e44b7fe2621a76d7fc1f975d89
Karl Lankester <kzdrenka@gmail.com>
2022-12-02 11:58:20 +0000

    day2

---
+++ day2.ts
@@ -0,0 +1,50 @@
+import fs from "fs";
+
+let score = 0;
+
+function generateScore(task: number, line: String) {
+  switch (line) {
+    case "A X":
+      task === 1 ? (score += 4) : (score += 3);
+      break;
+    case "A Y":
+      task === 1 ? (score += 8) : (score += 4);
+      break;
+    case "A Z":
+      task === 1 ? (score += 3) : (score += 8);
+      break;
+    case "B X":
+      score += 1;
+      break;
+    case "B Y":
+      score += 5;
+      break;
+    case "B Z":
+      score += 9;
+      break;
+    case "C X":
+      task === 1 ? (score += 7) : (score += 2);
+      break;
+    case "C Y":
+      task === 1 ? (score += 2) : (score += 6);
+      break;
+    case "C Z":
+      task === 1 ? (score += 6) : (score += 7);
+      break;
+  }
+}
+function day2Task(task: number) {
+  fs.readFile("resources/input_day2.txt", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const lines = data.split("\n");
+    lines.forEach((line) => {
+      generateScore(task, line);
+    });
+    console.log(score);
+  });
+}
+
+day2Task(2);
